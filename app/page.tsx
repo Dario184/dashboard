7b55a6851da090ee062cc511ea62a5932e6fe022
Dario184 <mangilidario@gmail.com>
2023-04-23 19:26:02 +0200

    impostati i componenti principali

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
 import { Card, Title, Text } from '@tremor/react';
-//import { queryBuilder } from '../lib/planetscale';
 import Search from './search';
-import UsersTable from './table';
 
 export const dynamic = 'force-dynamic';
 
-export default async function IndexPage({
-  searchParams
-}: {
-  searchParams: { q: string };
-}) {
-  // const search = searchParams.q ?? '';
-  // const users = await queryBuilder
-  //   .selectFrom('users')
-  //   .select(['id', 'name', 'username', 'email'])
-  //   .where('name', 'like', `%${search}%`)
-  //   .execute();
+export default async function IndexPage() {
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
---
+++ prisma/migrations/20230423154719_1_0/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NULL,
+    `username` VARCHAR(255) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `posts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `group` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
